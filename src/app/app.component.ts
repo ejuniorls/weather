f02912715b681ddb57f8888dddd6bd584fb360f7
Edmir Lôbo <ejuniorls@outlook.com>
2022-10-22 23:43:48 -0300

    fontawesome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faBootstrap, faAngular, faFontAwesome } from "@fortawesome/free-brands-svg-icons";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'weather';
+
+  faBootstrap = faBootstrap;
+  faAngular = faAngular;
+  faFontAwesome = faFontAwesome;
+
+
 }
