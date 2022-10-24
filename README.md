f6e04c08ed5df088f51071e3d859ea10945bb885
Edmir Lôbo <ejuniorls@outlook.com>
2022-10-23 22:58:33 -0300

    routes

--- README.md
+++ README.md
@@ -2,6 +2,11 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.4.
 
+## Rotas
+__home:__ http://localhost:4200
+
+__previsão do tempo:__ http://localhost:4200/previsao-do-tempo
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { WeatherForecastComponent } from './weather-forecast/weather-forecast.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherForecastComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
